5ef687dcec9e10b7667a64033318f15a50296ede
Chase16Rogers <chase16rogers@gmail.com>
2021-01-15 18:01:59 -0700

    got some account stuff in, trying to check further function

--- client/app/main.js
+++ client/app/main.js
@@ -1,13 +1,11 @@
 import { AuthController } from './Controllers/AuthController.js'
 import CommentsController from './Controllers/CommentsController.js'
 import FightsController from './Controllers/FightsController.js'
-import ProfilesController from './Controllers/ProfilesController.js'
 
 class App {
   authController = new AuthController();
 
   // client controllers
-  profilesController = new ProfilesController();
   commentsController = new CommentsController();
   fightsController = new FightsController();
 }
