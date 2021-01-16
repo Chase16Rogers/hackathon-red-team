ab29e146ec8f3d3ed8ccebcf6562219bf135afa7
CodyBryson <codybryson@hotmail.com>
2021-01-15 21:26:54 -0700

    fight model

--- client/app/Services/FightsService.js
+++ client/app/Services/FightsService.js
@@ -1,39 +1,32 @@
-import { ProxyState } from "../AppState.js";
-import Fight from "../Models/Fight.js";
-import { api, supersApi } from "./AxiosService.js";
+import { ProxyState } from '../AppState.js'
+import Fight from '../Models/Fight.js'
+import { api, supersApi } from './AxiosService.js'
 
 class FightsService {
+  async getFighters() {
+    console.log('Getting Fighters')
+    const res = await api.get('')
+    ProxyState.fighters = res.data.map(f => new Fight(f))
+  }
 
-    async getFighters() {
-        console.log("Getting Fighters")
-        let res = await api.get('')
-        ProxyState.fighters = res.data.map(f => new Fight(f))
+  async createFight() {
+    const res = await api.post('', ProxyState.fighters)
+    console.log(res)
+    ProxyState.fighters = [...ProxyState.fighters, new Fight(res.data)]
+  }
 
-    }
+  async editFight(fightId) {
+    const fight = ProxyState.fighters.find(fight => fight.id == fightId)
+    console.log(fight)
+  }
 
-
-    async createFight() {
-        let res = await api.post('', ProxyState.fighters);
-        console.log(res)
-        ProxyState.fighters = [...ProxyState.fighters, new Fight(res.data)]
-
-    }
-
-    async editFight(fightId) {
-        let fight = ProxyState.fighters.find(fight => fight.id == fightId)
-        console.log(fight)
-
-    }
-
-    async deleteFight(id) {
-        let res = await api.delete(ProxyState.fighters)
-        ProxyState.fighters = ProxyState.fighters.filter(f => f.id !== id)
-        ProxyState.activeFighterOne = null
-        ProxyState.activeFighterTwo = null
-    }
-
-
+  async deleteFight(id) {
+    const res = await api.delete(ProxyState.fighters)
+    ProxyState.fighters = ProxyState.fighters.filter(f => f.id !== id)
+    ProxyState.activeFighterOne = null
+    ProxyState.activeFighterTwo = null
+  }
 }
 
-export const fightsService = new FightsService();
-export default fightsService;
+export const fightsService = new FightsService()
+export default fightsService
